8e43890de406308e492940a9b92e048ef5f7a0a6
Valdarix <jwest@satori-assoc.com>
2021-04-15 17:00:01 -0400

    updated to unity alpha
    Update to new unity version

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,16 +6,17 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject _enemyOject;
-    [SerializeField]
-    private float _spawnTimer = 5.0f;
     private bool _enemyCanSpawn = true;
     [SerializeField]
     private GameObject _enemyContainer;
-    
+    [SerializeField]
+    private float _spawnTimer = 5.0f;
+    private WaitForSeconds _spawnWaitForSeconds;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnEnemy());
+        _spawnWaitForSeconds = new WaitForSeconds(_spawnTimer);
+        StartCoroutine("SpawnEnemy");
     }
 
     // Update is called once per frame
@@ -31,12 +32,13 @@ public class SpawnManager : MonoBehaviour
             Vector3 randomLocation = new Vector3(Random.Range(-8f, 8f), 7, 0);
             GameObject newEnemy = Instantiate(_enemyOject, randomLocation, Quaternion.identity);
             newEnemy.transform.parent = _enemyContainer.transform;
-            yield return new WaitForSeconds(_spawnTimer);
+            yield return _spawnWaitForSeconds;
         }
     }
 
     public void StopSpawning()
     {
         _enemyCanSpawn = false;
+        StopCoroutine("SpawnEnemy");
     }
 }
