04cfdaa49bc570bc4aedae615a233a451abc2006
Valdarix <jwest@satori-assoc.com>
2021-04-21 13:21:18 -0400

    Modular Powerups
    Start Modular Powerups

--- Assets/Scripts/PowerUpClass.cs
+++ Assets/Scripts/PowerUpClass.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Powerup : MonoBehaviour
-{   
+public class PowerUpClass : MonoBehaviour
+{
     [SerializeField]
-    private float _speed = 3.0f; 
+    private float _speed = 3.0f;
     [SerializeField]
     private int _powerUpID;
+    [SerializeField]
+    private Sprite _spriteIcon;
+    [SerializeField]
+    private Animator _animator;
 
 
     // Update is called once per frame
@@ -15,7 +19,7 @@ public class Powerup : MonoBehaviour
     {
         transform.Translate(Vector3.down * _speed * Time.deltaTime);
         if (transform.position.y < -8.0f)
-        {          
+        {
             Destroy(gameObject);
         }
     }
